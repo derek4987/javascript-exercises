62928de761d1d50ac57d2af2f7166ab619bde4ca
Derek Nunes <dereknunes28@gmail.com>
2022-02-10 22:45:05 -0600

    finish 04 using a nested for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let newArray = array;
+    let toRemove = remove;
 
+    for (let i = 0; i < toRemove.length; i++) {
+        let itemToRemove = toRemove[i];
+        for (let j = 0; j < newArray.length; j++) {
+            if (itemToRemove === newArray[j]) {
+                newArray.splice(j, 1);
+            } else {
+                continue;
+            }   
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
