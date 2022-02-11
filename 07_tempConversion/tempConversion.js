9515729c9e907e7bcb2ce4b5f38af569275eabdf
Derek Nunes <dereknunes28@gmail.com>
2022-02-11 01:13:11 -0600

    finish 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let celcius = (tempF - 32)*(5/9);
+  celcius = Math.round(celcius * 10) / 10;
+  return celcius;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let fahr = (tempC)*(9/5) + 32;
+  fahr = Math.round(fahr * 10) / 10;
+  return fahr;
 };
 
 // Do not edit below this line
