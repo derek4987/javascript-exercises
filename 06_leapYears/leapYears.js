7da2891b5a805be08e7f6bc5fd6332dbede5eb65
Derek Nunes <dereknunes28@gmail.com>
2022-02-11 01:00:46 -0600

    finish 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0 && year % 4 === 0)) {
+        const message = true;
+        return message;
+    } else if (year % 100 === 0) {
+        const message = false;
+        return message;
+    } else { 
+        return false;
+    }
 };
 
 // Do not edit below this line
