e579e3b06ac272f97304fb8a2ea4fc5077f907e4
Derek Nunes <dereknunes28@gmail.com>
2022-02-10 21:30:35 -0600

    finish 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, repeatNumber) {
+    let repeatString = ""
 
+    if (repeatNumber < 0) {
+        repeatString = 'ERROR';
+    }
+
+    for (let i=0; i<repeatNumber; i++) {
+        repeatString += string;
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
