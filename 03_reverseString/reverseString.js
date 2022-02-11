4abbc211d4fe968b1907364af2baa1ecb01f76fe
Derek Nunes <dereknunes28@gmail.com>
2022-02-10 21:51:53 -0600

    finish 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const newArray = Array.from((string.split('')));
+    newArray.reverse();
+    const reverseString = newArray.join('');
+    return reverseString;
 };
 
 // Do not edit below this line
