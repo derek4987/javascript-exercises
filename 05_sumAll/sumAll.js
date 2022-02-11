0819fd878fa0dfd6e58df14a74d99e513c0027d9
Derek Nunes <dereknunes28@gmail.com>
2022-02-11 00:34:48 -0600

    finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let smallNum;
+    let bigNum;
+    let result;
 
+    if (firstNum > secondNum) {
+        smallNum = secondNum;
+        bigNum = firstNum;
+    } else {
+        smallNum = firstNum;
+        bigNum = secondNum;
+    }
+
+    if (smallNum > 0 && bigNum >= 0 && typeof smallNum === "number" && typeof bigNum === "number") {
+        result = 0;
+        for (let i = smallNum; i <= bigNum; i++) {
+        result = result + i;
+        }  
+    } else {
+        result = "ERROR";
+    }
+
+    return result;
 };
 
 // Do not edit below this line
