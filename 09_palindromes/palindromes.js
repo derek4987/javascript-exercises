658effa5ee461c32be832ea35206437e0d7d2c5f
Derek Nunes <dereknunes28@gmail.com>
2022-02-19 16:59:13 -0600

    finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newStr = '';
+    let str = string.toLowerCase()
+        .replace(/[^\w\s]/g, "")
+        .replace(/\s/g, "");
+    str.split('');
+    for ( i = (str.length -1); i >= 0; i--) {
+        newStr = newStr + str[i];
+    }
+    if (str === newStr) { return true } else { return false };
 };
 
 // Do not edit below this line
