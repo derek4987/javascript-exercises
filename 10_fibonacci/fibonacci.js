bdf69485dd04c646171024c5aba14a54121e5bd8
Derek Nunes <dereknunes28@gmail.com>
2022-02-19 17:18:58 -0600

    finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(seqNbr) {
+    if (seqNbr < 0) { return 'OOPS'};
+    seqNbr = parseInt(seqNbr);
+    let fibArray = [1, 1];
+    for (i = 2; i < seqNbr; i ++) {
+        fibArray[i] = fibArray[i-1] + fibArray[i-2];
+    }
+    return fibArray[seqNbr-1];
 };
 
 // Do not edit below this line
