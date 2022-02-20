bcc6edf02a292f0f972ce707e253578ca19fd414
Derek Nunes <dereknunes28@gmail.com>
2022-02-19 18:32:07 -0600

    finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+// const findTheOldest = function(array) {
+//     const ordered = array.sort((a, b) => a.yearOfBirth < b.yearOfBirth ? 1 : -1);
+//     return ordered[2];
+//     const oldest = ordered.map(person => ordered.name);
+//     return oldest[0];
+// };
 
-};
+// solution from solutions branch
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+      const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+      return oldestAge < currentAge ? currentPerson : oldest;
+    })
+  };
+  
+  const getAge = function(birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
